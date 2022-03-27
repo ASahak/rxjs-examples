68ca1b9179ae4ce7c45a2ae1eb4bd0b4d8424cbd
ASahak <arthur.sahakyan96@gmail.com>
2022-03-27 19:39:21 +0400

    feat(operators): made bindCallback

---
+++ src/operators/bindCallback.js
@@ -0,0 +1,47 @@
+import { bindCallback, asyncScheduler } from 'rxjs';
+
+export default () => {
+    const func = (cb) => {
+        cb(5, 'some string', { someProperty: 'someValue' })
+    };
+
+    const boundSomeFunction = bindCallback(func);
+    boundSomeFunction().subscribe(values => {
+        console.log(values);
+    });
+
+
+    // With Async
+    function iCallMyCallbackSynchronously(cb) {
+        cb();
+    }
+
+    const boundSyncFn = bindCallback(iCallMyCallbackSynchronously);
+    const boundAsyncFn = bindCallback(iCallMyCallbackSynchronously, null, asyncScheduler);
+
+    boundSyncFn().subscribe(() => console.log('I was sync!'));
+    boundAsyncFn().subscribe(() => console.log('I was async!'));
+    console.log('This happened...');
+
+    // Logs:
+    // I was sync!
+    // This happened...
+    // I was async!
+
+
+    // Whit Object target
+    const someObj = {
+        getAge (cb) {
+            cb('I am 48 years old.')
+        }
+    }
+
+    const bindCallbackObjectFn = bindCallback(someObj.getAge)
+
+    bindCallbackObjectFn
+        .call(someObj)
+        .subscribe(console.log)
+
+    // :Log
+    // I am 48 years old.
+}
